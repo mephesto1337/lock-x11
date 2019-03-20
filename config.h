e0bc11307b78a1acfa0890c13a2c5d416b60c1be
Thomas WACHE <thomas.wache@intradef.gouv.fr>
2019-03-20 22:12:05 +0100

    Added macro to use slock or i3lock

--- config.h
+++ config.h
@@ -1,9 +1,16 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <stdlib.h>
+
 #define LOCK_FILENAME "/tmp/lock-x11.pid"
 #define DEFAULT_INACTIVITY "5m"
 
-static char *const lockcmd[] = { "/usr/bin/slock", "/usr/sbin/systemctl", "suspend", NULL };
+static char *const lockcmd[] =
+#ifdef USE_SLOCK
+{ "/usr/bin/slock", "/usr/sbin/systemctl", "suspend", NULL };
+#else
+{ "/usr/bin/i3lock", "-n", "-i", "/home/thomas/.config/i3/bg.png", "-e", "-f", NULL };
+#endif
 
 #endif // CONFIG_H
